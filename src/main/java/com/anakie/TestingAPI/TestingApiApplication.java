8eaa901a082ffe22392eb7a58f2b377c58ef0817
anakie <anakiem@mecerintered.co.za>
2024-05-14 11:01:01 +0200

    Initial commit

---
+++ src/main/java/com/anakie/TestingAPI/TestingApiApplication.java
@@ -0,0 +1,13 @@
+package com.anakie.TestingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anakie/TestingAPI/TestingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anakie.TestingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
